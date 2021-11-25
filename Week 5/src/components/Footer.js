39ce85d4cf1160aad49a106a7d1ba52a3f7ca033
Dimaetor <53823825+Coskntkk@users.noreply.github.com>
2021-11-25 19:27:19 +0300

    Update Footer.js

--- Week 5/src/components/Footer.js
+++ Week 5/src/components/Footer.js
@@ -1,6 +1,7 @@
 export default function Footer() {
     return (
         <footer className="info">
+            <p>Click to edit a todo</p>
             <p>Made with ❤️ by Coşkun Atak</p>
         </footer>
     );
